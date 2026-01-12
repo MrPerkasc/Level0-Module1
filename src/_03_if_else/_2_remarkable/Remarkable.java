261e8fce9e977a457f371e37519d52c00dd297b0
MrPerkasc <aliasharrison@gmail.com>
2026-01-12 18:58:35 +0000

    rawr

--- src/_03_if_else/_2_remarkable/Remarkable.java
+++ src/_03_if_else/_2_remarkable/Remarkable.java
@@ -8,11 +8,21 @@ public class Remarkable {
 public static void main(String[] args) {
 
 		// 1. Save something remarkable about each person in a variable.
+		String joeRemark = "Joe can play the guitar blindfolded.";
+		String bobRemark = "Bob can solve a Rubik's cube in under a minute.";
 
 		// 2. Ask the user to enter a name. Store their answer in a variable.
+		String name = JOptionPane.showInputDialog(null, "Enter a name:");
 
-		// 3. In a pop-up, tell the user what is remarkable about that person. 
+		// 3. In a pop-up, tell the user what is remarkable about that person.
+		if (name != null) {
+			if (name.equalsIgnoreCase("joe")) {
+				JOptionPane.showMessageDialog(null, joeRemark);
+			} else if (name.equalsIgnoreCase("bob")) {
+				JOptionPane.showMessageDialog(null, bobRemark);
+			} else {
+				JOptionPane.showMessageDialog(null, "I don't know anything remarkable about " + name + ".");
+			}
+		}
 
-	}
-}
-
+	}}
